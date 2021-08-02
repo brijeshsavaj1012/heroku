1a556b06fef4487a1d38a4d3acfb6b64d214f96e
brijeshsavaj1012 <savajbrijesh10@gmail.com>
2021-08-02 18:53:49 +0530

    add start

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose
-  .connect("mongodb+srv://lets_help:SXWJl2Dyn43toCZX@cluster0.roshd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/user", {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect("mongodb+srv://lets_help:SXWJl2Dyn43toCZX@cluster0.roshd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => {
     return('mongoose running');
   })
