0801f709fb5fbfe952cae70c96d6f15395099e4f
brijeshsavaj1012 <savajbrijesh10@gmail.com>
2021-08-02 16:56:49 +0530

    add start

--- routes/auth.js
+++ routes/auth.js
@@ -10,13 +10,14 @@ router.put('/signup',[
     body('email')
     .isEmail()
     .withMessage('please enter valid email.')
-    .custom((value, {req})=>{
-        return User.findOne({email: value}).then(userDoc => {
-            if(userDoc){
-                return Promise.reject('Email address already exists!');
-            }
-        })
-    }).normalizeEmail(),
+    // .custom((value, {req})=>{
+    //     return User.findOne({email: value}).then(userDoc => {
+    //         if(userDoc){
+    //             return Promise.reject('Email address already exists!');
+    //         }
+    //     })
+    // })
+    .normalizeEmail(),
     body('password').trim().isLength({min: 5}),
     body('name').trim().not().isEmpty()
 ],
