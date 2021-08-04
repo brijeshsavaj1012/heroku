3734231967d24a7689f3da497fa538e9dc130193
brijeshsavaj1012 <savajbrijesh10@gmail.com>
2021-08-04 15:37:32 +0530

    add start

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,14 @@ const { validationResult, Result } = require("express-validator");
 const User = require("../models/user");
 const HelpUser = require("../models/helpUser");
 exports.sign = (req,res,next)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation faild");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
